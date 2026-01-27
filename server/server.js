fdd82991ce6051e5263f6c3814ef914fc590a5cf
ABHIN M <abhin18m@gmail.com>
2026-01-27 19:29:17 +0530

    add db configs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
+require('dotenv').config();
+const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
-const PORT = 3000;
+
+connectDB();
 
 app.get('/',(req,res)=>{
     res.send("server is on");
-})
+});
+
 app.listen(PORT,() => {
     console.log("server started on port:", PORT)
 });
